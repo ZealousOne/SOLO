336eb9d698f9d82acabf9241d6c751b35f3c2f99
gzeal <unovan@protonmail.com>
2023-12-23 16:19:43 -0500

    working on clean up

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import _ from 'lodash';
 import './style.css';
 import Icon from './icon.jpg';
-
+import Data from './data.xml';
+import Notes from './data.csv';
 
 function component() {
   const element = document.createElement('div');
@@ -16,7 +17,9 @@ function component() {
 
   element.appendChild(myIcon);
 
-
+  console.log(Data);
+  console.log(Notes);
+  
   return element;
 }
 
