b39c9468e3038961ab4ed184a2fe4ddf9b4b3f17
gzeal <unovan@protonmail.com>
2023-12-23 17:13:24 -0500

    current error is additional properties

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
-const express = require('express'); 
+// const express = require('express'); 
+// const app = express(); 
+// const PORT = 3000; 
 
-const app = express(); 
-const PORT = 3000; 
+// app.get('/', (req, res)=>{ 
+// 	res.status(200); 
+// 	res.send("Welcome to root URL of Server"); 
+// }); 
 
-app.get('/', (req, res)=>{ 
-	res.status(200); 
-	res.send("Welcome to root URL of Server"); 
-}); 
+// app.listen(PORT, (error) =>{ 
+// 		if(!error) 
+// 		console.log(`Server is Successfully Running on PORT ${PORT}`); 
+// 		else
+// 		console.log("Error occurred, server can't start", error); 
+// 	} 
+// ); 
 
-app.listen(PORT, (error) =>{ 
-		if(!error) 
-		console.log(`Server is Successfully Running on PORT ${PORT}`); 
-		else
-		console.log("Error occurred, server can't start", error); 
-	} 
-); 
+// app.js
+const express = require('express');
+const path = require('path');
+const app = express();
 
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,41 @@
-const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-  module.exports = {
-    entry: {
-       app: './src/index.js',
-    },
-    devtool: 'inline-source-map',
-    // devtool: false,
-    devServer: {
-      static: './dist',
-     hot: true,
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        title: 'Hot Module Replacement',
-      }),
-    ],
-    output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-      clean: true,
-    },
-    // added the below to try to contain valid alias configuration
-    resolve: {
-      extensions: ['.tsx', '.ts', '.js'],
-    },
-    performance: {
-      hints: false,
-      maxEntrypointSize: 512000,
-      maxAssetSize: 512000
-    },
-    module: {
-      rules: [
-        {
-          test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
-        },      
-        {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: 'asset/resource',
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    host: 'localhost',
+    port: 8080,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+   publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
         },
-      ],
-    },
-  };
-
-
+      },
+    ],
+  },
+};
