8a3db8149c2ea0f617b626dea7ce932a355711b3
gzeal <unovan@protonmail.com>
2024-01-02 22:52:23 -0500

    npm run build works now

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,32 @@
+// import { resolve } from 'path';
+// import HtmlWebpackPlugin from 'html-webpack-plugin';
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
-  entry: {
-    index: './src/index.js',
-    print: './src/print.js',
+  mode :'development',
+  entry : {
+    index: './index.js'
   },
-  devtool: 'inline-source-map',
-  devServer: {
+
+  devtool : 'inline-source-map',
+  devServer : {
     static: './dist',
     host: 'localhost',
     port: 8080,
   },
-  plugins: [
+   plugins : [
     new HtmlWebpackPlugin({
       title: 'Development',
     }),
   ],
-  output: {
+  output : {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-   publicPath: '/',
+    publicPath: '/',
   },
-  module: {
+  module : {
     rules: [
       {
         test: /\.(js|jsx)$/,
@@ -37,5 +39,44 @@ module.exports = {
         },
       },
     ],
-  },
-};
+  }
+}
+
+
+// older code 
+
+// export const mode = 'development';
+// export const entry = {
+//   index: './index.js'
+// };
+// export const devtool = 'inline-source-map';
+// export const devServer = {
+//   static: './dist',
+//   host: 'localhost',
+//   port: 8080,
+// };
+// export const plugins = [
+//   new HtmlWebpackPlugin({
+//     title: 'Development',
+//   }),
+// ];
+// export const output = {
+//   filename: '[name].bundle.js',
+//   path: resolve(__dirname, 'dist'),
+//   clean: true,
+//   publicPath: '/',
+// };
+// export const module = {
+//   rules: [
+//     {
+//       test: /\.(js|jsx)$/,
+//       exclude: /node_modules/,
+//       use: {
+//         loader: 'babel-loader',
+//         options: {
+//           presets: ['@babel/preset-env', '@babel/preset-react'],
+//         },
+//       },
+//     },
+//   ],
+// };
