b2f20ba2778913f1dffdb3f757c4b640780aa843
gzeal <unovan@protonmail.com>
2024-01-02 21:17:03 -0500

    Trying to get react to react

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/', (req, res) => {
