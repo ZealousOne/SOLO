64646406c91b6301d14af67ed413f5d17c818f39
gzeal <unovan@protonmail.com>
2024-01-04 22:38:25 -0500

    before I change a lot in reference

--- app.js
+++ app.js
@@ -1,24 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { createRoot } from 'react-dom/client';
-import { useId } from 'react';
+// import { useId } from 'react';
 // app.js
 const express = require('express');
 const path = require('path');
 const app = express();
-
-
-// app.use(express.static(path.join(__dirname, 'dist')));
-
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-// });
-
-// const port = 3000;
-// app.listen(port, () => {
-//   console.log(`Server is running on http://localhost:${port}`);
-// });
-
-// added this to try and get something react to display
 
 const App = () => {
 
